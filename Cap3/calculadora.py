82e545ccf609987bfbd42cc771488246fe12561f
Vinícius Jovito <61995014+JovitoVinicius@users.noreply.github.com>
2025-01-31 11:07:06 -0300

    Add files via upload

---
+++ Cap3/calculadora.py
@@ -0,0 +1,5 @@
+print("Bem-vindo ao Calculador de área de um Paralelogramo")
+base = int(input("Qual a base deseja calcular?"))
+altura = int(input("Qual altura deseja calcular?"))
+area = base * altura
+print("A área do paralelogamo é de " ,area , "metros quadrados")
